e2a9dbad5c91cb2199030d0f33604fb9835e3a96
mamuche <7558@holbertonstudents.com>
2023-10-17 06:41:18 -0700

    8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print numbers 0 to 16.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
