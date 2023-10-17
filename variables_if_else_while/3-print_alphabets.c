7f132ecc6b22b09001b120a058164b04269af6a6
mamuche <7558@holbertonstudents.com>
2023-10-17 03:00:39 -0700

    2-print_alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print alphabet lowercase and uppercase
+ *
+ *Return: alway 0
+ */
+
+int main(void)
+{
+	int alphabet = (('a')('A'));
+	while (alphabet <= ('z' 'Z'))
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+		return (0);
+}
