119ea17e17498a7cfa3c53a02ae03cbb40c4e8d4
mamuche <7558@holbertonstudents.com>
2023-10-17 05:47:10 -0700

    6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- *main - print single digit numbers of base 10
- *
- *Return: always 0
- */
+ /**
+  *main - print single digit numbers of base 10
+  *
+  *Return: always 0
+  */
 
 int main(void)
 {
 	int singleDigit = 0;
 
-	while (singleDigit <= 10)
+	while (singleDigit < 10)
 	{
 		putchar(singleDigit + '0');
-		if (singleDigit < 9)
-		{
-			putchar('\n');
-		}
 		singleDigit++;
 	}
 	putchar('\n');
-		
+
 	return (0);
 }
