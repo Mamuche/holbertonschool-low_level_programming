f146be6c5ac6651326f425cdb3f93ecbb94424c5
mamuche <7558@holbertonstudents.com>
2023-10-18 10:27:37 -0700

    8-24_hours

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of number.
+ *
+ * Return: the value of the last digit.
+ */
+
+int print_last_digit(int n)
+{
+	_putchar("");
+
+	return ("n % 10");
+}
--- functions_nested_loops/8-main.c
+++ functions_nested_loops/8-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet_10x();
+	jack_bauer();
 	return (0);
 }
