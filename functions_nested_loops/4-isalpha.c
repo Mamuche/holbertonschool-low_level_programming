45df5a1b865a5444f6dfae98c91e21fcdc955896
mamuche <7558@holbertonstudents.com>
2023-10-18 05:34:05 -0700

    4-isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,10 +9,10 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'a'&& c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 
-	if (c >= 'A'&& c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 
 	return (0);
