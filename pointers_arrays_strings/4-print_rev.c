3b6cacfa6f8ce5c4102aa0487130b3b62b6a9fda
mamuche <7558@holbertonstudents.com>
2023-10-24 01:47:36 -0700

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse.
+ * @s: pointeur to string.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	for (i = i - 1; i > 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
