e1f2a564ea7fc4b87b612ebfc0650417fe899fe8
mamuche <7558@holbertonstudents.com>
2023-10-27 01:23:43 -0700

    5-string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change lowercase of a string to uppercase.
+ * @ptr: pointer to string.
+ *
+ * Return: pointer to string.
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+      	return (str);
+}
