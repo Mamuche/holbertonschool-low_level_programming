1589d65ef2d21febc6a131348066b34ec2d7994b
mamuche <7558@holbertonstudents.com>
2023-10-31 06:40:51 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * @argc: number of cmd line arguments.
+ * @argv: array of size argc.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
