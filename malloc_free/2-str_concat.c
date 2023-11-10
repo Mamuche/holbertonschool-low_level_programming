e4b93ef76d0373095847257452c91c3cf8e84699
mamuche <7558@holbertonstudents.com>
2023-11-10 05:14:06 -0800

    2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,11 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int i = 0, j = 0, k;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
