70b08be1bc248306dbb319f8a5c30cfbd9286d1f
mamuche <7558@holbertonstudents.com>
2023-11-17 08:15:53 -0800

    3-print_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+typedef struct method
+{
+	char letter;
+	void (*print)(va_list ap);
+} choice;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
