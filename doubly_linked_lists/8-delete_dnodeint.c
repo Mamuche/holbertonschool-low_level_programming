f67d69fe18e03eb16b290963348780360f1e849f
Mamuche <marionlaroche17@gmail.com>
2023-12-01 17:00:07 +0100

    Fix task 8 when delete first node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -19,6 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if ((*head) != NULL)
+			(*head)->prev == NULL;
 		free(tmp);
 		return (1);
 	}
