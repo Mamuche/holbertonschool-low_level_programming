c8f4cfa25faad1e060054ba98bd0ab0463bbb745
Mamuche <marionlaroche17@gmail.com>
2023-12-04 15:56:36 +0100

    task0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout.
+ * @filename: pointer to the string to print.
+ * @letters: the number of letters it should read and print.
+ *
+ * Return: the actual number of letters it should read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+	return (w);
+
+	if (fd == -1 || r == -1 || w == -1)
+		return (0);
+}
