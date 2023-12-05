a4ddf32d9dae605e59d1ebbe04e7cc1e31258cbe
Mamuche <marionlaroche17@gmail.com>
2023-12-05 14:35:41 +0100

    correction task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,9 +10,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	char *buf;
 	unsigned int len = 0;
-	int r, w, fd;
+	int w, fd;
 
 	if (filename == NULL)
 	return (-1);
@@ -23,19 +22,12 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	buf = malloc(sizeof(char) * len);
-	if (buf == NULL)
-		return (0);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(STDOUT_FILENO, text_content, fd);
 
-	fd = open(filename, O_CREAT, O_RDWR, O_TRUNC, S_IWUSR);
-	r = read(fd, buf, len);
-	w = write(STDOUT_FILENO, buf, r);
-
-	free(buf);
-
-	if (fd == -1 || r == -1 || w == -1)
+	if (fd == -1 || w == -1)
 	{
-		return (0);
+		return (-1);
 	}
 
 	close(fd);
